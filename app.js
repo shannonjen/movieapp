4b4d0d7ec8d3b04a6876d45f1c706fff5d1ae86e
Jennifer Shannon <jen.shannon@gmail.com>
2017-01-23 14:44:41 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var pg = require('pg');
+var DB_URL = 'postgres://localhost:5432/movieapp'
+
+app.set('view engine', 'ejs');
+app.set('views','./views' )
+
+app.get('/movies', function(req, res){
+  pg.connect(DB_URL, function(err,response,done){
+    response.query(`select * from movies`, function(err, result){
+      res.render("index", { movies: result.rows })
+    })
+  })
+})
+
+app.listen(3000, function(){
+  console.log("You are listening on port 3000")
+})
