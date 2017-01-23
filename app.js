6eaa36ed973b4eae5a09eec514d64a94797144a6
Jennifer Shannon <jen.shannon@gmail.com>
2017-01-23 14:51:09 -0500

    changed db url

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
-var DB_URL = 'postgres://localhost:5432/movieapp'
+// var DB_URL = 'postgres://localhost:5432/movieapp'
 
 app.set('view engine', 'ejs');
 app.set('views','./views' )
 
 app.get('/movies', function(req, res){
-  pg.connect(DB_URL, function(err,response,done){
-    response.query(`select * from movies`, function(err, result){
-      res.render("index", { movies: result.rows })
-    })
-  })
+  // pg.connect(DB_URL, function(err,response,done){
+  //   response.query(`select * from movies`, function(err, result){
+  //     res.render("index", { movies: result.rows })
+  //   })
+  // })
+  res.send("Hello TTP!")
 })
 
 app.listen(3000, function(){
